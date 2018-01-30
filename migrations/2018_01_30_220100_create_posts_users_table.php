e5b2468209aecb637e692545c72f3b77f0f068cd
Daniël Klabbers <daniel@klabbers.email>
2018-01-30 22:57:15 +0100

    foreign cant work with myisam

--- migrations/2018_01_30_220100_create_posts_users_table.php
+++ migrations/2018_01_30_220100_create_posts_users_table.php
@@ -18,7 +18,6 @@ return [
             $table->integer('post_id')->unsigned();
             $table->integer('user_id')->unsigned();
 
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     },
